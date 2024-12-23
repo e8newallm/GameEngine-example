c09bdf91be9d1272e26de649204c6498f576ed3d
Matthew Newall <matthewnewall@hotmail.com>
2024-12-23 23:52:37 +0000

    Add unhandled exception handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <filesystem>
+#include <string>
 #include <thread>
 
 #include "logging.h"
@@ -8,9 +9,27 @@ extern int debugConsole();
 
 bool gameClosing = false;
 
+void unhandledException()
+{
+    try
+    {
+        std::rethrow_exception(std::current_exception());
+    }
+    catch (std::exception const &e)
+    {
+        std::string err = std::string("Unhandled exception: ") + e.what();
+        Logger::error(err);
+        std::cout << err << std::endl;
+    }
+
+    std::abort();
+}
+
 int main()
 {
     Logger::init(std::filesystem::temp_directory_path().append("gamelog.txt"));
+    std::set_terminate(unhandledException);
+
     std::thread mainThread(game);
 
 #ifdef DEBUG
