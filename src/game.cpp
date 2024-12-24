89c55383b9d7d4ea62811c23a2412e012c6f0ba0
Matthew Newall <matthewnewall@hotmail.com>
2024-12-24 09:42:30 +0000

    Use Window class instead of direct SDL_Window calls

--- src/game.cpp
+++ src/game.cpp
@@ -3,8 +3,10 @@
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_thread.h>
 
+#include <SDL_events.h>
 #include <ctime>
 
+#include "window.h"
 #include "physicsobject.h"
 #include "image.h"
 #include "world.h"
@@ -26,18 +28,13 @@ int game()
     {
         printf("error initializing SDL: %s\n", SDL_GetError());
     }
-    SDL_Window* win = SDL_CreateWindow("GAME", // creates a window
-                                       SDL_WINDOWPOS_CENTERED,
-                                       SDL_WINDOWPOS_CENTERED,
-                                       1000, 1000, 0);
-    Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
-    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+    Window mainWindow("GAME", 1000, 1000, 0);
 
     PackageManager dataPackage("data.bin");
     View viewport( {1000, 1000}, {0, 0});
     viewport.setZoom(1.0);
 
-    World world(rend, &viewport);
+    World world(mainWindow.getRend(), &viewport);
 
     for(std::string filename : dataPackage.getFileList())
     {
@@ -46,7 +43,7 @@ int game()
             std::vector<uint8_t> data = dataPackage.getFile(filename);
             SDL_RWops* dataBuffer = SDL_RWFromMem(data.data(), data.size());
             SDL_Surface* surf = IMG_Load_RW(dataBuffer, 1);
-            Texture::add(SDL_CreateTextureFromSurface(rend, surf), filename);
+            Texture::add(SDL_CreateTextureFromSurface(mainWindow.getRend(), surf), filename);
             SDL_FreeSurface(surf);
         }
     }
@@ -54,13 +51,16 @@ int game()
     world.addImage(new Image({0, 0, 1000, 1000}, "/background.png", UINT8_MAX));
     world.addPhyObj(new PhysicsObject({0, 960, 1000, 40}, PHYOBJ_STATIC | PHYOBJ_COLLIDE, new Texture("/Tile.png")));
 
-    world.addPhyObj(new Player({500, 920, 40, 40}, PHYOBJ_COLLIDE, new SpriteMap(rend, &dataPackage, "/spritemap.json")));
+    world.addPhyObj(new Player({500, 920, 40, 40}, PHYOBJ_COLLIDE, new SpriteMap(mainWindow.getRend(), &dataPackage, "/spritemap.json")));
     world.startPhysics();
 
     while (!gameClosing)
     {
         MouseState::update();
         KeyState::update();
+
+        if(KeyState::key(SDL_SCANCODE_Q) == SDL_KEYDOWN)
+            gameClosing = true;
 
         SDL_Event event;
         while (SDL_PollEvent(&event))
@@ -91,15 +91,10 @@ int game()
             newPosition.x += delta.x / viewport.getZoom();
             viewport.setPosition(newPosition);
         }
-
-        SDL_RenderClear(rend);
-        world.draw();
-        SDL_RenderPresent(rend);
+        mainWindow.render(world);
     }
 
     world.stopPhysics();
-    SDL_DestroyRenderer(rend);
-    SDL_DestroyWindow(win);
     SDL_Quit();
 
     return 0;
